68df17e73720bab483a0de6d571cf9f90e3721cf
Gryphos272 <ariekert@uni-muenster.de>
2025-02-12 22:45:48 +0100

    Moved plotting functions to plotting.py

--- plotting.py
+++ plotting.py
@@ -6,9 +6,11 @@ from matplotlib.ticker import FuncFormatter
 from datetime import datetime
 
 
+def kms(x, _):
+    return f'{x * 1e-6:.1f}km'  # Converts to kilometres
+
+
 def plot_locs_and_vehicles(locations, vehicles):
-    def kms(x, _):
-        return f'{x * 1e-6:.1f}km'  # Converts to kilometres
 
     fig, ax = plt.subplots(figsize=(14, 12))
     ax.scatter([loc.coordinates[0] for loc in locations.values()],
@@ -18,13 +20,88 @@ def plot_locs_and_vehicles(locations, vehicles):
     for car in vehicles.values():
         x = float(car.loc.coordinates[0])
         y = float(car.loc.coordinates[1])
-        print(f'Start location of vehicle {car.name}: ({x}, {y}).')
         ax.plot(x, y, marker='x', label=car.name)
     ax.legend()
     ax.xaxis.set_major_formatter(FuncFormatter(kms))
     ax.yaxis.set_major_formatter(FuncFormatter(kms))
 
     ax.set_title('Locations and starting points of carriers')
+
+    return fig, ax
+
+
+def plot_nr_jobs(vehicles, orders):
+    carrier_dict = {car: 0 for car in vehicles.keys()}
+    for order in orders.values():
+        carrier_dict[order.pick_carrier[0].name] += 1
+
+    fig, ax = plt.subplots(figsize=(14, 7))
+    ax.bar(*zip(*carrier_dict.items()))
+
+    ax.set_xlabel('Carrier')
+    ax.set_ylabel('Number of jobs')
+    ax.set_title('Number of completed jobs per carrier')
+
+    return fig, ax
+
+
+def plot_distance(vehicles):
+    fig, ax = plt.subplots(figsize=(14, 7))
+    ax.bar(vehicles.keys(), [car.travelled_distance() for car in vehicles.values()])
+
+    ax.set_xlabel('Carrier')
+    ax.set_ylabel('Distance travelled')
+    ax.set_title('Distance travelled by carriers')
+
+    ax.yaxis.set_major_formatter(FuncFormatter(kms))
+
+    return fig, ax
+
+
+def plot_work_percentage(vehicles):
+    total_times = [(car.actions[-1].end_time - car.log_on_time).total_seconds() for car in vehicles.values()]
+    durations = [car.pick_duration() + car.drop_duration() + car.drive_duration() for car in vehicles.values()]
+    percentages = [100. * dur / total_time for (dur, total_time) in zip(durations, total_times)]
+
+    fig, ax = plt.subplots(figsize=(14, 7))
+    ax.bar(vehicles.keys(), percentages)
+
+    ax.set_xlabel('Carrier')
+    ax.set_ylabel('Percentage of time working')
+    ax.set_title('Percentage of time carriers are working')
+
+    return fig, ax
+
+
+def plot_overlaps(vehicles):
+    fig, ax = plt.subplots(figsize=(14, 7))
+    ax.bar(vehicles.keys(), [car.big_overlaps for car in vehicles.values()], color='red', label='Overlap of >1s')
+    ax.bar(vehicles.keys(), [car.overlaps for car in vehicles.values()],
+           bottom=[car.big_overlaps for car in vehicles.values()], color='orange', label='Overlap of 1s')
+    ax.set_xlabel('Carrier')
+    ax.set_ylabel('Number of overlaps')
+    ax.set_title('Number of action overlaps per carrier')
+    ax.legend()
+
+    return fig, ax
+
+
+def plot_action_times(vehicles):
+    pick_durations = [car.pick_duration() for car in vehicles.values()]
+    drop_durations = [car.drop_duration() for car in vehicles.values()]
+    travel_durations = [car.drive_duration() for car in vehicles.values()]
+
+    fig, ax = plt.subplots(figsize=(14, 7))
+
+    ax.bar(vehicles.keys(), pick_durations, label='Pick', color='red')
+    ax.bar(vehicles.keys(), drop_durations, label='Drop', color='blue', bottom=pick_durations)
+    ax.bar(vehicles.keys(), travel_durations, label='Drive', color='green',
+           bottom=[sum(x) for x in zip(pick_durations, drop_durations)])
+
+    ax.set_xlabel('Carrier')
+    ax.set_ylabel('Work time (s)')
+    ax.set_title('Total working time of carriers')
+    ax.legend()
 
     return fig, ax
 
--- regex.py
+++ regex.py
@@ -1,7 +1,6 @@
-import re
 from read_files import *
 from structs import *
-from plotting import plot_locs_and_vehicles
+from plotting import *
 
 locations, carriers, orders = read_data_to_dicts('VOSimu-InputInformation.xlsx')
 
@@ -13,106 +12,23 @@ print('Number of times position information was wrong:', errors['wrong_location'
 print('Number of times distance information was wrong:', errors['wrong_distance'])
 print('Number of incomplete/failed jobs:', errors['incomplete_jobs'])
 
+calculate_overlaps(carriers)
+check_consistency(carriers)
 
-def kms(x, _):
-    return f'{x * 1e-6:.1f}km'  # Converts to kilometres
+ax0, fig0 = plot_nr_jobs(carriers, orders)
 
+fig2, ax2 = plot_work_percentage(carriers)
 
-fig, ax = plt.subplots(figsize=(14, 14))
+fig3, ax3 = plot_distance(carriers)
 
-for i, act in enumerate(carriers['SC001'].actions):
-    ax.plot(*act.origin.coordinates, marker='x')
-    ax.annotate(f'Action {i + 1}', act.origin.coordinates)
-    # print('Start:', act.origin.name, 'End:', act.dest.name, 'Start time:', act.start_time, 'End time:', act.end_time, 'Distance:', act.dist)
+fig4, ax4 = plot_action_times(carriers)
 
-ax.set_title('Actions of carrier SC001')
-fig1, ax1 = plt.subplots(figsize=(14, 7))
-fig2, ax2 = plt.subplots(figsize=(14, 7))
-fig3, ax3 = plt.subplots(figsize=(14, 7))
-fig4, ax4 = plt.subplots(figsize=(14, 7))
-fig5, ax5 = plt.subplots(figsize=(14, 7))
-
-time_overlaps = 0
-big_time_overlaps = 0
-
-for i, car in enumerate(carriers.values()):
-    dur = timedelta(0)
-    pick_dur = 0
-    drop_dur = 0
-    drive_dur = 0
-    dist = 0
-    total_time = car.actions[-1].end_time - car.log_on_time
-    for act in car.actions:
-        dur += act.duration
-        dist += act.origin.dist(act.dest)
-        if act.type == 'PICK':
-            pick_dur += act.duration.total_seconds()
-        elif act.type == 'DROP':
-            drop_dur += act.duration.total_seconds()
-        else:
-            drive_dur += act.duration.total_seconds()
-    percentage = 100. * dur.total_seconds() / total_time.total_seconds()
-
-    ax1.bar(car.name, len(car.actions))
-    ax2.bar(car.name, percentage)
-    ax3.bar(car.name, dist)
-    if i == 0:
-        ax4.bar(car.name, pick_dur, label='Pick', color='red')
-        ax4.bar(car.name, drop_dur, label='Drop', bottom=pick_dur, color='blue')
-        ax4.bar(car.name, drive_dur, label='Drive', bottom=drop_dur + pick_dur, color='green')
-    else:
-        ax4.bar(car.name, pick_dur, color='red')
-        ax4.bar(car.name, drop_dur, bottom=pick_dur, color='blue')
-        ax4.bar(car.name, drive_dur, bottom=drop_dur + pick_dur, color='green')
-    for i, act in enumerate(car.actions[:-1]):
-        next_act = car.actions[i + 1]
-        if act.end_time > next_act.start_time + timedelta(seconds=1):
-            car.big_overlaps += 1
-        elif act.end_time > next_act.start_time:
-            car.overlaps += 1
-
-        if next_act.type == 'DROP':
-            assert act.type == 'DRIVE_DROP' and act.order == next_act.order
-        if next_act.type == 'PICK':
-            assert act.type == 'DRIVE_PICK' and act.order == next_act.order
-        if next_act.type == 'DRIVE_DROP':
-            assert act.type == 'PICK' and act.order == next_act.order
-        if next_act.type == 'DRIVE_PICK':
-            assert act.type == 'DROP'
-
-
-ax1.set_xlabel('Carrier')
-ax1.set_ylabel('Number of actions')
-ax1.set_title('Number of actions per carrier')
-
-ax2.set_xlabel('Carrier')
-ax2.set_ylabel('Percentage of time working')
-ax2.set_title('Work load of carriers')
-
-ax3.set_xlabel('Carrier')
-ax3.set_ylabel('Distance travelled')
-ax3.set_title('Distance travelled by carriers')
-
-ax4.set_xlabel('Carrier')
-ax4.set_ylabel('Work time (s)')
-ax4.set_title('Total working time of carriers')
-ax4.legend()
-
-
-ax5.bar([car.name for car in carriers.values()], [car.big_overlaps for car in carriers.values()], color='red', label='Overlap of >1s')
-ax5.bar([car.name for car in carriers.values()], [car.overlaps for car in carriers.values()], bottom=[car.big_overlaps for car in carriers.values()], color='orange', label='Overlap of 1s')
-ax5.set_xlabel('Carrier')
-ax5.set_ylabel('Number of overlaps')
-ax5.set_title('Number of action overlaps per carrier')
-ax5.legend()
+fig5, ax5 = plot_overlaps(carriers)
 
 ax3.yaxis.set_major_formatter(FuncFormatter(kms))
-
-ax.xaxis.set_major_formatter(FuncFormatter(kms))
-ax.yaxis.set_major_formatter(FuncFormatter(kms))
 
 fig6, ax6 = plot_locs_and_vehicles(locations, carriers)
 fig5.savefig('plot_overlaps')
 fig6.savefig('plot_locs')
-#.legend()
+
 plt.show()
