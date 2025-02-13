d815ee32f00ed01874536804ade093843beb7e24
Gryphos272 <ariekert@uni-muenster.de>
2025-02-13 12:01:04 +0100

    Also plot initial covered distances.

--- testing.py
+++ testing.py
@@ -2,9 +2,9 @@ from read_files import *
 from structs import *
 from plotting import *
 
-locations, carriers, orders = read_data_to_dicts('VOSimu-InputInformation.xlsx')
+locations, carriers, orders = read_data_to_dicts('data/VOSimu-InputInformation.xlsx')
 
-locations, carriers, orders, errors = read_logs('logger_all.log', locations, carriers, orders)
+locations, carriers, orders, errors = read_logs('data/logger_all.log', locations, carriers, orders)
 
 # print total number of orders and number of things that went wrong (should all be 0)
 print('Number of orders:', errors['nr_orders'])
@@ -28,11 +28,14 @@ fig5, ax5 = plot_overlaps(carriers)
 
 fig6, ax6 = plot_locs_and_vehicles(locations, carriers)
 
-fig1.savefig('plot_nr_jobs')
-fig2.savefig('plot_work_percentage')
-fig3.savefig('plot_distances')
-fig4.savefig('plot_action_times')
-fig5.savefig('plot_overlaps')
-fig6.savefig('plot_locs')
+fig7, ax7 = plot_first_distances(carriers)
+
+fig1.savefig('plots/plot_nr_jobs')
+fig2.savefig('plots/plot_work_percentage')
+fig3.savefig('plots/plot_distances')
+fig4.savefig('plots/plot_action_times')
+fig5.savefig('plots/plot_overlaps')
+fig6.savefig('plots/plot_locs')
+fig7.savefig('plots/plot_initial_distances')
 
 plt.show()
