d6c5e46ee87851edd10013500dd8f68cb8b93c89
Gryphos272 <ariekert@uni-muenster.de>
2025-02-13 11:06:11 +0100

    Saved all figures, removed unneeded stuff.

--- testing.py
+++ testing.py
@@ -6,6 +6,7 @@ locations, carriers, orders = read_data_to_dicts('VOSimu-InputInformation.xlsx')
 
 locations, carriers, orders, errors = read_logs('logger_all.log', locations, carriers, orders)
 
+# print total number of orders and number of things that went wrong (should all be 0)
 print('Number of orders:', errors['nr_orders'])
 print('Number of times capacity was exceeded:', errors['capacity_exceeded'])
 print('Number of times position information was wrong:', errors['wrong_location'])
@@ -15,7 +16,7 @@ print('Number of incomplete/failed jobs:', errors['incomplete_jobs'])
 calculate_overlaps(carriers)
 check_consistency(carriers)
 
-ax0, fig0 = plot_nr_jobs(carriers, orders)
+fig1, ax1 = plot_nr_jobs(carriers, orders)
 
 fig2, ax2 = plot_work_percentage(carriers)
 
@@ -25,9 +26,12 @@ fig4, ax4 = plot_action_times(carriers)
 
 fig5, ax5 = plot_overlaps(carriers)
 
-ax3.yaxis.set_major_formatter(FuncFormatter(kms))
-
 fig6, ax6 = plot_locs_and_vehicles(locations, carriers)
+
+fig1.savefig('plot_nr_jobs')
+fig2.savefig('plot_work_percentage')
+fig3.savefig('plot_distances')
+fig4.savefig('plot_action_times')
 fig5.savefig('plot_overlaps')
 fig6.savefig('plot_locs')
 
